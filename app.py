934f73edd3fa79d61112c18866bd35e13abcee30
Shumbusho43 <davidshumbusho10@gmail.com>
2025-07-27 20:01:12 +0200

    created basic app and handled db connection

---
+++ app.py
@@ -0,0 +1,69 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access an environment variable
+dbname = os.getenv("DATABASE")
+db_host = os.getenv("HOST")
+# db_user = os.getenv("USER")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("PASSWORD")
+
+
+# Global variables
+mydb = None
+welcome_message = """--------------------------------------
+    SAVR Financial Management Tool
+--------------------------------------
+Welcome SAVR
+A Management Tool for Small-Scale Entrepreneurs!
+This tool helps you track income, expenses, budgets, and savings goals."""
+
+menu_message = """Please Select an option:
+1. Add Income
+2. Add Expense
+3. Set Budget
+4. Set Savings Goal
+5. View Financial Summary
+6. Exit
+"""
+
+# connecting to database
+mydb = None
+try:
+    mydb = mysql.connector.connect(
+        host=db_host, user=db_user, password=db_password, database=dbname
+    )
+except mysql.connector.Error:
+    print("Failed to connect to the database, Please check your connection credentials")
+    exit()
+
+
+def main():
+    if mydb:
+        print(welcome_message)
+        while True:
+            print(menu_message)
+            choice = input("Enter your choice: ").strip()
+            if choice == "1":
+                print("Adding Income...")
+            elif choice == "2":
+                print("Adding Expense...")
+            elif choice == "3":
+                print("Setting Budget...")
+            elif choice == "4":
+                print("Setting Savings Goal...")
+            elif choice == "5":
+                print("Viewing Financial Summary...")
+            elif choice == "6":
+                print("Exiting SAVR. Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
